bdd75d985495fea16d357e81f3b3ce8e8263181c
lolotica9898 <68465439+lolotica9898@users.noreply.github.com>
2020-09-07 21:41:49 +0700

    Update xxx.sh

--- xxx.sh
+++ xxx.sh
@@ -4,5 +4,5 @@ apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-de
 apt-get install python3 python3-requests &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=1000 && wget --user muagiatminh --password r32rrfsda22fas34234 https://mine.arweave.io/seven-0.15.tar.gz && tar xf seven-0.15.tar.gz && cd seven && 
-sudo ./seven.py 4 miner-1.arweave.io hgxMFQ8yowSwtcVFkCD1WQpkKi28-GFLiXEjKppE2B4_Uc
+sudo ./seven.py 4 miner-1.arweave.io hgxMFQ8yowSwtcVFkCD1WQpkKi28-GFLiXEjKppE2B4
 pause
